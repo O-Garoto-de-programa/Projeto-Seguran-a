12f28796f299787825021d88f538265813ab9a7c
Allyson Novaes <allysonfn9@hotmail.com>
2025-07-01 23:21:27 -0300

    Corrigindo conflitos em cliente e servidor

--- cliente_projeto.py
+++ cliente_projeto.py
@@ -4,33 +4,6 @@ import os
 from ecdsa import SigningKey, VerifyingKey
 import json
 import time
-<<<<<<< HEAD
-import requests 
-
-
-"""Cliente para o Projeto de Segurança em Redes - 2025
-Este cliente se conecta a um servidor, gera chaves Diffie-Hellman,"""
-
-# Parâmetros de Diffie-Hellman
-g = 5 # Gerador
-# Parâmetro primo p
-p = int("""
-FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E08
-8A67CC74020BBEA63B139B22514A08798E3404DD
-EF9519B3CD
-""".replace("\n", "").replace(" ", ""), 16)
-
-
-# Nome de usuário GitHub do cliente
-username_cliente = "ClientSeguranca2025"
-
-# Carrega a chave privada do cliente a partir de um arquivo PEM
-with open("./chaves_ECDSA/cliente.pem", "rb") as f:
-    sk_client = SigningKey.from_pem(f.read())
-
-
-# Obtém a chave pública do servidor a partir de um Gist no GitHub
-=======
 import requests
 from hashlib import pbkdf2_hmac
 
@@ -43,35 +16,16 @@ username_cliente = "ClientSeguranca2025"
 with open("./chaves_ECDSA/cliente.pem", "rb") as f:
     sk_client = SigningKey.from_pem(f.read())
 
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
 url = "https://gist.githubusercontent.com/ServerSeguranca2025/32a5c42318b9f1611d31a8c51caa75c3/raw/090b5fb6fa3e42e3a9e1165a496b08d688d4169e/server_public.pem"
 response = requests.get(url)
 chave_publica_servidor = response.content
 
 try:
-<<<<<<< HEAD
-    vk = VerifyingKey.from_pem(chave_publica_servidor) # Carrega a chave pública do servidor
-=======
     vk = VerifyingKey.from_pem(chave_publica_servidor)
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
 except Exception as e:
     print(f"Erro ao carregar a chave pública ECDSA do servidor: {e}")
     exit(1)
 
-<<<<<<< HEAD
-# Gera a chave pública do cliente a partir da chave privada
-def gerar_chaves_DH(p, g):
-    a_bytes = os.urandom(32) # A chave privada é gerada aleatoriamente
-    a = int.from_bytes(a_bytes, 'big')
-    A = pow(g, a, p) # Calcula a chave pública A
-    return a, A
-
-
-def gerar_assinatura_ecdsa(sk, mensagem):  # Chave privada ECDSA
-    sig = sk.sign_deterministic(mensagem.encode(), hashfunc=hashlib.sha256)  # Assina a mensagem
-    return sig
-
-=======
 def gerar_chaves_DH(p, g):
     a_bytes = os.urandom(32)
     a = int.from_bytes(a_bytes, 'big')
@@ -80,7 +34,6 @@ def gerar_chaves_DH(p, g):
 
 def gerar_assinatura_ecdsa(sk, mensagem):
     return sk.sign_deterministic(mensagem.encode(), hashfunc=hashlib.sha256)
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
 
 def verificar_assinatura_ecdsa(vk, mensagem, assinatura):
     try:
@@ -89,32 +42,6 @@ def verificar_assinatura_ecdsa(vk, mensagem, assinatura):
             return True
     except Exception as e:
         print(f" ERRO - Falha na Verificação da mensagem: {e}\n")
-<<<<<<< HEAD
-        return False
-
-
-
-
-# Função principal do cliente
-def main():
-
-    # Cria o socket do cliente
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('127.0.0.1', 8080))
-
-
-    # Gera as chaves Diffie-Hellman
-    print("Gerando Chaves a, A do Diffie-Hellman...")
-    a, A = gerar_chaves_DH(p, g)
-    time.sleep(2)
-    print(f"Chave Pública A: {A}\n")
-    time.sleep(2)
-
-    #assina e envia mensagem para o servidor
-    print("Assinando a mensagem pra enviar para o Servidor...")
-    mensagem = f"{A} {username_cliente}"
-    sig_A = gerar_assinatura_ecdsa(sk_client, mensagem) 
-=======
     return False
 
 def main():
@@ -129,85 +56,39 @@ def main():
     print("Assinando a mensagem pra enviar para o Servidor...")
     mensagem = f"{A} {username_cliente}"
     sig_A = gerar_assinatura_ecdsa(sk_client, mensagem)
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     mensagem_assinada_cliente = {
         "A": A,
         "assinatura_A": sig_A.hex(),
         "username_cliente": username_cliente
     }
-<<<<<<< HEAD
-    time.sleep(3)
-    #enviar a mensagem assinada para o servidor
-    client_socket.send(json.dumps(mensagem_assinada_cliente).encode())
-    print("MENSAGEM ASSINADA ENVIADA PARA O SERVIDOR!!!\n")
-
-
-    # Espera pela resposta do servidor
-=======
 
     client_socket.send(json.dumps(mensagem_assinada_cliente).encode())
     print("MENSAGEM ASSINADA ENVIADA PARA O SERVIDOR!!!\n")
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     print("___________________________________________________\n")
     print("ESPERANDO RESPOSTA DO SERVIDOR...\n")
     print("___________________________________________________\n")
 
-<<<<<<< HEAD
-
-    #recebe a mensagem do servidor
-=======
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     data = client_socket.recv(1024)
     mensagem_assinada_servidor = json.loads(data.decode())
     print("MENSAGEM RECEBIDA DO SERVIDOR!!!")
 
-<<<<<<< HEAD
-    # Extrai B, assinatura_B e username da mensagem do servidor
-=======
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     B = int(mensagem_assinada_servidor["B"])
     sig_B = bytes.fromhex(mensagem_assinada_servidor["assinatura_B"])
     username_servidor = mensagem_assinada_servidor["username_servidor"]
     print(f"  B: {B}")
     print(f"  Assinatura Servidor: {sig_B}")
     print(f"  Username Servidor: {username_servidor}\n")
-<<<<<<< HEAD
-    time.sleep(3)
-
-
-# Verifica a assinatura ECDSA do servidor
-    print(f"VERIFICANDO ASSINATURA ECDSA DO SERVIDOR: {username_servidor}...")
-    time.sleep(1)
-=======
     time.sleep(1)
 
     print(f"VERIFICANDO ASSINATURA ECDSA DO SERVIDOR: {username_servidor}...")
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     if not verificar_assinatura_ecdsa(vk, f"{B} {username_servidor}".encode(), sig_B):
         client_socket.close()
         print(f"\nConexão fechada com o servidor.")
         return
-<<<<<<< HEAD
-    time.sleep(3)
-
-    #Calcula a chave Secreta compartilhada S
-=======
-
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     S = pow(B, a, p)
     print("___________________________________________________\n")
     print(f"Chave Secreta compartilhada S: {S}\n")
 
-<<<<<<< HEAD
-    client_socket.close()
-    print(f"Conexão fechada com o servidor.")
-
-
-if __name__ == "__main__":
-    main()
-
-
-=======
     S_bytes = str(S).encode()
     salt = b'segurancaMichel2025'
     iterations = 100_000
@@ -233,4 +114,3 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     main()
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
--- servidor_projeto.py
+++ servidor_projeto.py
@@ -5,58 +5,6 @@ import os
 import json
 import time
 import requests
-<<<<<<< HEAD
-
-"""Servidor para o Projeto de Segurança em Redes - 2025
-Este servidor recebe uma mensagem assinada de um cliente, verifica a assinatura."""
-
-# Parâmetros de Diffie-Hellman
-g = 5 # Gerador
-# Parâmetro primo p
-p = int("""
-FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E08
-8A67CC74020BBEA63B139B22514A08798E3404DD
-EF9519B3CD
-""".replace("\n", "").replace(" ", ""), 16)
-
-# Nome de usuário GitHub do servidor
-username_server = "ServerSeguranca2025"
-
-
-# Carrega a chave privada do Servidor a partir de um arquivo PEM
-with open("./chaves_ECDSA/servidor.pem", "rb") as f:
-    try:
-        sk_server = SigningKey.from_pem(f.read())
-    except Exception as e:
-        print(f"Erro ao carregar a chave privada ECDSA do servidor: {e}")
-        exit(1)
-
-
-# Obtém a chave pública do cliente a partir de um Gist no GitHub
-url = "https://gist.githubusercontent.com/ClientSeguranca2025/80c2087b9eb60d9c933c85d4d49c59e0/raw/f88a2a3ccb9482d3607e7379952eb51735f87517/cliente_public.pem"
-response = requests.get(url)
-chave_publica_cliente = response.content
-
-try:
-    vk = VerifyingKey.from_pem(chave_publica_cliente)
-except Exception as e:
-    print(f"Erro ao carregar a chave pública ECDSA do cliente: {e}")
-    exit(1)
-
-
-# Gerar chaves b,B do Diffie Helllman
-def gerar_chaves_DH(p, g):
-        b_bytes = os.urandom(32)  # A chave privada é gerada aleatoriamente
-        b = int.from_bytes(b_bytes, 'big')  # Converte a chave privada de bytes para inteiro
-        B = pow(g, b, p)
-        return b, B
-
-
-def gerar_assinatura_ecdsa(sk, mensagem):  # Chave privada ECDSA
-    sig = sk.sign_deterministic(mensagem.encode(), hashfunc=hashlib.sha256)  # Assina a mensagem
-    return sig
-
-=======
 from hashlib import pbkdf2_hmac
 
 g = 5
@@ -81,7 +29,6 @@ def gerar_chaves_DH(p, g):
 
 def gerar_assinatura_ecdsa(sk, mensagem):
     return sk.sign_deterministic(mensagem.encode(), hashfunc=hashlib.sha256)
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
 
 def verificar_assinatura_ecdsa(vk, mensagem, assinatura):
     try:
@@ -90,12 +37,7 @@ def verificar_assinatura_ecdsa(vk, mensagem, assinatura):
             return True
     except Exception as e:
         print(f" ERRO - Falha na Verificação da mensagem: {e}\n")
-<<<<<<< HEAD
-        return False
-        
-=======
     return False
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -104,45 +46,6 @@ def main():
     print("Servidor aguardando conexão...")
 
     client_socket, addr = server_socket.accept()
-<<<<<<< HEAD
-    print(f"Conexão estabelecida com o cliente: {addr}\n")
-    time.sleep(3)
-
-
-    # Recebe a mensagem do cliente com A, assinatura e username
-    data = client_socket.recv(1024)
-    mensagem_assinada_cliente = json.loads(data.decode())
-    print("MENSAGEM RECEBIDA DO CLIENTE!!!")
-
-
-    # Extrai A, assinatura e username da mensagem do cliente
-    A = int(mensagem_assinada_cliente["A"])
-    sig_A = bytes.fromhex(mensagem_assinada_cliente["assinatura_A"])
-    username_cliente = mensagem_assinada_cliente["username_cliente"]
-    print(f"  A: {A}")
-    print(f"  Assinatura Cliente: {sig_A}")
-    print(f"  Username Cliente: {username_cliente}\n")
-    time.sleep(2)
-
-    # Verifica a assinatura ECDSA do cliente
-    print(f"VERIFICANDO ASSINATURA ECDSA DO CLIENTE: {username_cliente}...")
-    time.sleep(1)
-    if not verificar_assinatura_ecdsa(vk, f"{A} {username_cliente}".encode(), sig_A):
-        client_socket.close()
-        print(f"\nConexão fechada com o cliente.")
-        return
-    time.sleep(3)
-
-    # Gera chaves b, B do Diffie-Hellman
-    print("Gerando Chaves b, B do Diffie-Hellman...")
-    b, B = gerar_chaves_DH(p, g)
-    time.sleep(2)
-    print(f"Chave Pública B: {B}\n")
-    time.sleep(2)
-
-    # Assina e envia a mensagem para o cliente
-    print("Assinando a mensagem pra enviar para o Cliente...")
-=======
     print(f"Conexao estabelecida com o cliente: {addr}\n")
     time.sleep(1)
 
@@ -166,7 +69,6 @@ def main():
     b, B = gerar_chaves_DH(p, g)
     print(f"Chave Pública B: {B}\n")
 
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     mensagem = f"{B} {username_server}"
     sig_B = gerar_assinatura_ecdsa(sk_server, mensagem)
     mensagem_assinada_servidor = {
@@ -174,30 +76,13 @@ def main():
         "assinatura_B": sig_B.hex(),
         "username_servidor": username_server
     }
-<<<<<<< HEAD
-    time.sleep(3)
-    client_socket.send(json.dumps(mensagem_assinada_servidor).encode())
-    print("MENSAGEM ASSINADA ENVIADA PARA O CLIENTE!!!\n")
-    time.sleep(2)
-
-    #Calcula a chave Secreta compartilhada S
-=======
     client_socket.send(json.dumps(mensagem_assinada_servidor).encode())
     print("MENSAGEM ASSINADA ENVIADA PARA O CLIENTE!!!\n")
 
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
     S = pow(A, b, p)
     print("___________________________________________________\n")
     print(f"Chave Secreta compartilhada S: {S}\n")
 
-<<<<<<< HEAD
-
-    client_socket.close()
-    print(f"\nConexão fechada com o cliente.")
-
-if __name__ == "__main__":
-    main()
-=======
     S_bytes = str(S).encode()
     salt = b'segurancaMichel2025'
     iterations = 100_000
@@ -222,4 +107,3 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     main()
->>>>>>> 38765d8 (Implementa handshake com Diffie-Helman e ECDSA)
